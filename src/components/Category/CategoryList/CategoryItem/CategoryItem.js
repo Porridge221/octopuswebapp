d72439de2e74f6b5e5815602c54337187952f84e
Yuri Ivanov <ivanov.u97@gmail.com>
2023-08-26 12:12:10 +1000

    add middleware for validate data with tg initData

--- src/components/Category/CategoryList/CategoryItem/CategoryItem.js
+++ src/components/Category/CategoryList/CategoryItem/CategoryItem.js
@@ -1,11 +1,14 @@
 import styles from './CategoryItem.module.css';
+import useTelegram from '../../../../hooks/useTelegram';
 
 function CategoryItem({item}) {
+
+  const {initData} = useTelegram();
   
   const fetchData = () => {
     fetch("https://octopusbot-1-k6943301.deta.app/createorder", { method:'POST',headers: {
       'Content-Type': 'application/json',
-      'Telegram-Data': 'query_id=AAH4j_ZmAgAAAPiP9mZgcz3Q&user=%7B%22id%22%3A6022402040%2C%22first_name%22%3A%22Debora%22%2C%22last_name%22%3A%22Landau%22%2C%22username%22%3A%22porridge221%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1692761838&hash=19a8574d0d965d89b7608c5eb5c6855c958eb28e88cb150fcea80c87596b72f4'
+      'Telegram-Data': initData
     }, body: JSON.stringify( item )
       })
       .then(response => {
--- src/components/Category/CategoryList/CategoryList.js
+++ src/components/Category/CategoryList/CategoryList.js
@@ -2,12 +2,14 @@ import CategoryItem from './CategoryItem/CategoryItem';
 import styles from './CategoryList.module.css';
 import { useEffect, useState } from "react"
 import useTelegram from '../../../hooks/useTelegram';
+//import axios from 'axios'
 
 function CategoryList() {
 
   const [items, setItems] = useState([])
 
   const {initData} = useTelegram();
+  //const initData = 'query_id=AAH4j_ZmAgAAAPiP9mbhemjf&user=%7B%22id%22%3A6022402040%2C%22first_name%22%3A%22Debora%22%2C%22last_name%22%3A%22Landau%22%2C%22username%22%3A%22porridge221%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1692771816&hash=9515f6c19bbf76fc35b29df2eb0bcd97d93ade207b37238ae145abc5d7fc28ce'
 
   const fetchData = () => {
     fetch("https://octopusbot-1-k6943301.deta.app/variants", {method: 'GET', headers: {'Content-Type': 'application/json', 'Telegram-Data': initData}})
@@ -21,7 +23,14 @@ function CategoryList() {
 
   useEffect(() => {
     fetchData();
-  }, [])
+
+    // axios({url: "http://localhost:8000/variants", method: "GET", headers: {'Telegram-Data': 'fdgdfg'}}).then((resp) => {
+    //   const allItems = resp.data;
+    //   setItems(allItems);
+    // });
+
+    // eslint-disable-next-line
+  }, [initData])
 
   return (
     items.length > 0 ? (
