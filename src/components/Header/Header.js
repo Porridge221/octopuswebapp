f487334efb25e8905667b1a36f21d56043316f00
Yuri Ivanov <ivanov.u97@gmail.com>
2023-09-13 12:47:48 +1000

    tg theme categories, header, loyalty

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,11 @@ function Header({path, current}) {
                 <span className={styles.textCurrent}>{current}</span>
             </div>
             <Link className={styles.CartLink} to='/cart'>
-                <img className={styles.icon} src={process.env.PUBLIC_URL + '/assets/Group 25.svg'} alt=''/>
+                <svg width="31" height="24" viewBox="0 0 31 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="Group 25">
+                        <path id="Vector 16" className={styles.icon} d="M8.19672 8.92H2L4.65574 23H26.7869L29 8.92H22.8033M8.19672 8.92H22.8033M8.19672 8.92C8.19672 8.92 7.14286 0.999992 15.2787 1C23.4145 1.00001 22.8033 8.92 22.8033 8.92" stroke="#424242" stroke-width="2"/>
+                    </g>
+                </svg>
             </Link>
         </div>
     );
