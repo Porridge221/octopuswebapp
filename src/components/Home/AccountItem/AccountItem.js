59e7d60b233686bd15d35fb08c9f985d85e9f5f3
Yuri Ivanov <ivanov.u97@gmail.com>
2023-09-13 12:03:45 +1000

    tg theme in account page

--- src/components/Home/AccountItem/AccountItem.js
+++ src/components/Home/AccountItem/AccountItem.js
@@ -21,7 +21,12 @@ function AccountItem({user_data}) {
                 <div className={styles.PointsCount}>{user_data !== undefined && user_data.user.discount_points} баллов</div>
             </Link>
             <Link className={styles.CartLink} to='/cart'>
-                <img className={styles.CartButton} src={process.env.PUBLIC_URL + '/assets/Group 25.svg'} alt=''/>
+            <svg width="31" height="24" viewBox="0 0 31 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Group 25">
+            <path id="Vector 16" className={styles.CartButton} d="M8.19672 8.92H2L4.65574 23H26.7869L29 8.92H22.8033M8.19672 8.92H22.8033M8.19672 8.92C8.19672 8.92 7.14286 0.999992 15.2787 1C23.4145 1.00001 22.8033 8.92 22.8033 8.92" stroke="#424242" stroke-width="2"/>
+            </g>
+            </svg>
+                {/* <img className={styles.CartButton} src={process.env.PUBLIC_URL + '/assets/Group 25.svg'} alt=''/> */}
             </Link>
         </div>
     );
--- src/components/Home/OrderList/OrderList.js
+++ src/components/Home/OrderList/OrderList.js
@@ -78,7 +78,7 @@ function OrderList({user_data}) {
                 </div>
                 <div className={modalStyles.VerticalBox}>
                     <span style={{fontWeight: '600'}}>Пункт выдачи:</span>
-                    <span style={{color:'#424242'}}>г. Владивосток, ул. Русская, 25. Ежедневно с 10:00 до 21:00</span>
+                    <span style={{color:'var(--tg-theme-text-color)'}}>г. Владивосток, ул. Русская, 25. Ежедневно с 10:00 до 21:00</span>
                     <span>Товары: {modalItemsCount > 0 &&  (modalItemsCount + ' шт.')}</span>
                     <div className={modalStyles.HorizontalBox}>
                         <span>Итог</span>
