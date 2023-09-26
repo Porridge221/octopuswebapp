9d426dea8ba1c443d81b11d369f3a94ea242a2ac
Yuri Ivanov <ivanov.u97@gmail.com>
2023-09-27 01:01:34 +1000

    add sakhoctopus tg link, consultant button logic on homepage

--- src/components/Home/CategoryList/CategoryItem/CategoryItem.js
+++ src/components/Home/CategoryList/CategoryItem/CategoryItem.js
@@ -1,10 +1,23 @@
+import useTelegram from '../../../../hooks/useTelegram'
 import styles from './CategoryItem.module.css'
 import {Link} from 'react-router-dom'
 
 function CategoryItem({category}) {
+    const {tg, initData} = useTelegram()
+
+    const fetchData = () => {
+        fetch("https://octopus-vape.ru/consultant", {method: 'GET', headers: {'Content-Type': 'application/json', 'Telegram-Data': initData,}})
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            console.log(data);
+            tg.close()
+          })
+      }
     
     return (
-        <Link className={styles.root} to={category.link}>
+        <Link className={styles.root} onClick={() => category.id === 2 && fetchData()} to={category.link}>
             <div className={category.style + ' ' + styles.unnamed6}>
                 <div className={category.style !== 'TelegramRef' ? styles.textBlock : styles.textBlock2}>{category.text}</div>
                 {category.style === 'ChooseAndOrder' && <img className={styles.CartButton} src={process.env.PUBLIC_URL + '/assets/home_order_icon.svg'} alt=''/>}
--- src/components/Home/CategoryList/CategoryList.js
+++ src/components/Home/CategoryList/CategoryList.js
@@ -3,7 +3,7 @@ import styles from "./CategoryList.module.css"
 
 function CategoryList({user_data}) {
     const category = [
-        {id: 1, style: 'TelegramRef', text: 'Наш Telegram-канал:\n@octopus', link: user_data?.user?.city_id === 3 ? 'https://t.me/CreaGeese' : 'https://t.me/octopusvl'},
+        {id: 1, style: 'TelegramRef', text: 'Наш Telegram-канал:\n@octopus', link: user_data?.user?.city_id === 3 ? 'https://t.me/octopussakh' : 'https://t.me/octopusvl'},
         {id: 2, style: 'HelpConsultant', text: 'Помощь консультанта', link: '/home/categories'},
         {id: 3, style: 'ChooseAndOrder', text: 'Выбрать и заказать', link: '/home/categories'}
     ]
