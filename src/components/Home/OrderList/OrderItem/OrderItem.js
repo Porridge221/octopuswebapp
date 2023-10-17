ac2cf326f8e8704926260559a8f94ab094d3aa46
Yuri Ivanov <ivanov.u97@gmail.com>
2023-10-18 09:42:33 +1000

    ### replace order id with order name ###

--- src/components/Home/OrderList/OrderItem/OrderItem.js
+++ src/components/Home/OrderList/OrderItem/OrderItem.js
@@ -6,7 +6,7 @@ function OrderItem({order, handleSetModal}) {
         <div className={styles.root} onClick={() => handleSetModal(true, order)}>
             <img className={styles.icon} src={process.env.PUBLIC_URL + '/assets/Group 48.svg'} alt=''/>
             <div className={styles.Info} >
-                <div className={styles.header}>Заказ №{order.order_id}</div>
+                <div className={styles.header}>Заказ №{order.name}</div>
                 <div className={styles.price}>Итог: {new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', minimumFractionDigits: 0}).format(order.price/100)}</div>
             </div>
         </div> 
--- src/components/OrderHistory/OrderItem/OrderItem.js
+++ src/components/OrderHistory/OrderItem/OrderItem.js
@@ -4,7 +4,7 @@ function OrderItem({order, handleSetModal}) {
     
     return (
         <div className={styles.root} onClick={() => handleSetModal(true, order)}>
-            <div className={styles.header}>Заказ №{order.order_id}</div>
+            <div className={styles.header}>Заказ №{order.name}</div>
             <img className={styles.icon} src={process.env.PUBLIC_URL + '/assets/Group 48.svg'} alt=''/>
             <div className={styles.price}>Итог: {new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', minimumFractionDigits: 0}).format(order.price/100)}</div>
         </div>
