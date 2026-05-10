44da9f8c1eadafbab218c285d8c53cd8e362ad6e
porridge221 <ivanov.u97@gmail.com>
2026-05-10 20:10:02 +1000

    [FIX] upd tg links

--- src/components/Home/CategoryList/CategoryList.js
+++ src/components/Home/CategoryList/CategoryList.js
@@ -3,7 +3,7 @@ import styles from "./CategoryList.module.css"
 
 function CategoryList({user_data}) {
     const category = [
-        {id: 1, style: 'TelegramRef', text: 'Наш Telegram-канал:\n@octopus', link: [3, 4, 5].indexOf(user_data?.user?.city_id) !== -1 ? 'https://t.me/+oWkDbzLcfrM1Njgy' : 'https://t.me/+oWkDbzLcfrM1Njgy'},
+        {id: 1, style: 'TelegramRef', text: 'Наш Telegram-канал:\n@octopus', link: [3, 4, 5].indexOf(user_data?.user?.city_id) !== -1 ? 'https://t.me/+5cmCvehbyFIwZGYy' : 'https://t.me/+oWkDbzLcfrM1Njgy'},
         {id: 2, style: 'HelpConsultant', text: 'Помощь консультанта', link: '/home/categories'},
         {id: 3, style: 'ChooseAndOrder', text: 'Выбрать и заказать', link: '/home/categories'}
     ]
